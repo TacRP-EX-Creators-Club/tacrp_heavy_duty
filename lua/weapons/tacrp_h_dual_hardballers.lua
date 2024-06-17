49d6affac895ffa18320fa7594656cd74f2a42ac
TheOnly8Z <888888zombies@gmail.com>
2024-06-17 16:20:21 -0700

    remove reload multiplier from TTT stats

--- lua/weapons/tacrp_h_dual_hardballers.lua
+++ lua/weapons/tacrp_h_dual_hardballers.lua
@@ -52,8 +52,6 @@ SWEP.BalanceStats = {
             [HITGROUP_RIGHTLEG] = 0.75,
             [HITGROUP_GEAR] = 0.9
         },
-
-        ReloadTimeMult = 1.25 * 0.85,
     },
     [TacRP.BALANCE_PVE] = {
         Damage_Max = 22,
